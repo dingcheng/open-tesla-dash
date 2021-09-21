cf82cf7ac3ee7515399c87c038e02e99479d7a5e
dingcheng <topren1@gmail.com>
2021-09-20 23:59:32 -0700

    Fix test

--- infra/test/data-stream.test.ts
+++ infra/test/data-stream.test.ts
@@ -1,13 +1,13 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as Infra from '../lib/datastream-stack';
+import { DataStreamStack } from '../lib/datastream-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new Infra.InfraStack(app, 'MyTestStack');
+    const stack = new DataStreamStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
-    }, MatchStyle.EXACT))
+    }, MatchStyle.SUPERSET))
 });
