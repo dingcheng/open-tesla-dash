9dc03f9c484b02af0f1fbeaae709f6360ec0decd
dingcheng <topren1@gmail.com>
2021-09-21 00:33:00 -0700

    install latest cdk

--- infra/lib/cdkpipelines-stack.ts
+++ infra/lib/cdkpipelines-stack.ts
@@ -20,8 +20,8 @@ export class CdkPipelineStack extends Stack {
 
                 // Install dependencies, build and run cdk synth
                 commands: [
+                    'npm install -g aws-cdk',
                     'cd infra',
-                    'cdk --version',
                     'npm ci',
                     'npm run build',
                     'npx cdk synth'
